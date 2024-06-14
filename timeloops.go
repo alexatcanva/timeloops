f4dcd3726586b1a0df03f445cb72320958a78cdc
alexbathome <alexbennettece@gmail.com>
2024-06-14 22:39:16 +0800

    improved coverage

--- timeloops.go
+++ timeloops.go
@@ -4,23 +4,50 @@ import (
 	"time"
 )
 
-// ForDuration is a function that runs a function for a specified number of
-// times or until a specified time has passed.
+// ForDuration will perform some action in a loop for a given time.Duration, once
+// the timer expires, or once n loops have been completed this function will
+// return
 func ForDuration(n int, duration time.Duration, fn func()) {
-	var bf func(n int) bool = func(n int) bool { return false }
+	bf := breakFuncFactory(n)
+	timer := time.NewTimer(duration)
+	forDurationChan(n, bf, timer.C, fn)
+}
+
+// ForTimer will perform some action in a loop for a given *time.Timer, once
+// the timer expires, or once n loops have been completed this function will
+// return
+func ForTimer(n int, timer *time.Timer, fn func()) {
+	forDurationChan(n, breakFuncFactory(n), timer.C, fn)
+}
+
+// breakFuncFactory will generate a function to break the loop
+// dependening on whether or not a count (n) is provided.
+// if n is <= 0, this is ignored.
+func breakFuncFactory(n int) func(n int) bool {
+	bf := func(n int) bool { return false }
 	if n > 0 {
 		bf = func(n int) bool {
 			return n == 0
 		}
 	}
-	timer := time.NewTimer(duration)
+	return bf
+}
+
+// forDurationChan handles breaking the loop based off a read
+// only time.Time chan.
+var forDurationChan = func(
+	n int,
+	bfn func(n int) bool,
+	stopChan <-chan time.Time,
+	fn func(),
+) {
 Loop:
 	for {
-		if bf(n) {
+		if bfn(n) {
 			break
 		}
 		select {
-		case <-timer.C:
+		case <-stopChan:
 			break Loop
 		default:
 			fn()
--- timeloops_test.go
+++ timeloops_test.go
@@ -1,7 +1,6 @@
 package timeloop
 
 import (
-	"math"
 	"testing"
 	"time"
 )
@@ -10,23 +9,40 @@ func TestCount(t *testing.T) {
 	count := 5
 	results := 0
 	ForDuration(count, 5*time.Second, func() {
-		print("hello")
 		results += 1
 	})
-
 	if results != 5 {
 		t.Fatal("did not reach expected count")
 	}
 }
 
-func TestTimeNaive(t *testing.T) {
-	before := time.Now()
-	ForDuration(0, 5*time.Second, func() {
-		print("hello")
-	})
-	after := time.Now()
-	got := math.Floor(after.Sub(before).Seconds())
-	if got != 5 {
-		t.Fatalf("expected %f, got %f", 5.0, got)
+func TestBreakFuncFactory(t *testing.T) {
+	n := 3
+	bf := breakFuncFactory(n)
+	count := 0
+	for !bf(n) {
+		count++
+		n--
+	}
+	if count != 3 {
+		t.Fatalf("expected %d, got %d", 3, count)
+	}
+}
+
+func TestForDuringChan(t *testing.T) {
+	tc := make(chan time.Time, 1)
+	x := func(n int) bool { return false }
+
+	ticks := 0
+	fn := func() {
+		ticks++
+		if ticks == 2 {
+			tc <- time.Now()
+		}
+	}
+	forDurationChan(3, x, tc, fn)
+
+	if ticks != 2 {
+		t.Fatalf("expected: %d, got: %d", 2, ticks)
 	}
 }
