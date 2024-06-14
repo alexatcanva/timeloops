8ec6175059b65ad0a5529b72927495eaf8333f65
alexbathome <alexbennettece@gmail.com>
2024-06-14 22:45:44 +0800

    100% coverage

--- timeloops_test.go
+++ timeloops_test.go
@@ -5,10 +5,21 @@ import (
 	"time"
 )
 
-func TestCount(t *testing.T) {
+func TestCountDuration(t *testing.T) {
 	count := 5
 	results := 0
 	ForDuration(count, 5*time.Second, func() {
+		results += 1
+	})
+	if results != 5 {
+		t.Fatal("did not reach expected count")
+	}
+}
+
+func TestCountTimer(t *testing.T) {
+	count := 5
+	results := 0
+	ForTimer(count, time.NewTimer(5*time.Second), func() {
 		results += 1
 	})
 	if results != 5 {
@@ -26,6 +37,33 @@ func TestBreakFuncFactory(t *testing.T) {
 	}
 	if count != 3 {
 		t.Fatalf("expected %d, got %d", 3, count)
+	}
+}
+
+func TestBreakFuncFactoryNoN(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		n      int
+		expect bool
+	}{
+		{
+			desc:   "0 n",
+			n:      0,
+			expect: false,
+		},
+		{
+			desc:   "-1 n",
+			n:      -1,
+			expect: false,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			res := breakFuncFactory(tC.n)(tC.n)
+			if res != tC.expect {
+				t.Fatalf("expected: %t, got %t", tC.expect, res)
+			}
+		})
 	}
 }
 
